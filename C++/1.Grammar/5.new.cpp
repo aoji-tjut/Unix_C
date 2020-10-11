b838b851d2588fe6673c124a19f990115bbf610e
aoji-tjut <245611640@qq.com>
2020-10-11 12:29:41 +0800

    10.11

--- C++/1.Grammar/5.new.cpp
+++ C++/1.Grammar/5.new.cpp
@@ -9,25 +9,47 @@ int main()
     cout << endl;
 
     //1D
-    int* q = new int[5];    //返回数组首地址 数组共5个元素
+    int* arr1 = new int[5];    //返回数组首地址 数组共5个元素
     for(int i = 0; i < 5; i++)
     {
-        q[i] = i;
+        arr1[i] = i;
     }
     for(int i = 0; i < 5; i++)
     {
-        cout << q + i << " " << *(q + i) << endl;
+        cout << arr1 + i << " " << *(arr1 + i) << endl;
     }
-    delete[] q;
+    delete[] arr1;
     cout << endl;
 
     //2D
     int row = 2;
     int col = 3;
-    int** r = new int* [row];
+    int (* arr2_1)[3] = new int[row][3];    //必须常量
     for(int i = 0; i < row; i++)
     {
-        r[i] = new int[col];
+        static int k = 0;
+
+        for(int j = 0; j < col; j++)
+        {
+            arr2_1[i][j] = ++k;
+        }
+    }
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            cout << &arr2_1[i][j] << " " << arr2_1[i][j] << "\t";
+        }
+
+        cout << endl;
+    }
+    delete[] arr2_1;
+    cout << endl;
+
+    int** arr2_2 = new int* [row];  //内存不连续
+    for(int i = 0; i < row; i++)
+    {
+        arr2_2[i] = new int[col];
     }
     for(int i = 0; i < row; i++)
     {
@@ -35,24 +57,24 @@ int main()
 
         for(int j = 0; j < col; j++)
         {
-            r[i][j] = ++k;
+            arr2_2[i][j] = ++k;
         }
     }
     for(int i = 0; i < row; i++)
     {
         for(int j = 0; j < col; j++)
         {
-            cout << &r[i][j] << " " << r[i][j] << "\t";
+            cout << &arr2_2[i][j] << " " << arr2_2[i][j] << "\t";
         }
 
         cout << endl;
     }
     for(int i = 0; i < row; i++)
     {
-        delete[] r[i];
+        delete[] arr2_2[i];
     }
-    delete[] r;
-    
+    delete[] arr2_2;
+
     //delete后可以继续当作栈区指针或堆区指针重新赋值使用
 
     return 0;
