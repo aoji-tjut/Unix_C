574e45d718e7ac53719a574e0e1c9d4c9e68ee33
aoji-tjut <245611640@qq.com>
2020-11-01 21:37:02 +0800

    11.1

--- C++/test.cpp
+++ C++/test.cpp
@@ -26,22 +26,57 @@
 #include <tuple>
 using namespace std;
 
+string mostCommonWord(string paragraph, vector<string>& banned)
+{
+    map<string, int> m_s;
+    map<string, int> m_v;
+    map<int, string, greater<>> res;
+
+    for(auto i :banned)
+    {
+        m_v[i]++;
+    }
+
+    string temp;
+    for(int i = 0; i < paragraph.size(); i++)
+    {
+        if(paragraph[i] >= 'A' && paragraph[i] <= 'Z')
+        {
+            paragraph[i] = tolower(paragraph[i]);
+        }
+
+        if(paragraph[i] >= 'a' && paragraph[i] <= 'z')
+        {
+            temp.push_back(paragraph[i]);
+        }
+        else
+        {
+            if(m_v.find(temp) == m_v.end() && !temp.empty())
+            {
+                m_s[temp]++;
+            }
+
+            temp.clear();
+        }
+    }
+
+    if(m_v.find(temp) == m_v.end())
+    {
+        m_s[temp]++;
+    }
+
+    for(auto i :m_s)
+    {
+        res[i.second] = i.first;
+    }
+
+    return res.begin()->second;
+}
+
 int main()
 {
-    int size;
-    cin >> size;
-
-    int a[size];
-    for(int i = 0; i < size; i++)
-    {
-        a[i] = i;
-    }
-    for(int i:a)
-    {
-        cout << i << " ";
-    }
-
-
+    vector<string> v{"bob", "hit"};
+    cout << mostCommonWord("Bob. hIt, baLl", v);
 
     return 0;
 }
