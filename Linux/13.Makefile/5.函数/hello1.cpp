fcde7c7fb6ae10045f7263757d5f19af14ac9bfa
aoji-tjut <245611640@qq.com>
2021-03-26 12:19:33 +0800

    3.26

---
+++ Linux/13.Makefile/5.函数/hello1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"hello1.h"
+using namespace std;
+
+void hello1()
+{
+	cout << "hello1" << endl;
+}
---
+++ Linux/13.Makefile/5.函数/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"hello1.h"
+#include"hello2.h"
+using namespace std;
+
+int main()
+{
+	cout << "main" << endl;
+	hello1();
+	hello2();
+
+	return 0;
+}
