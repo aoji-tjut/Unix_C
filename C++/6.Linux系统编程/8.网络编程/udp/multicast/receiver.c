ef0c6591bee2e5ee0774164747da8d956e99b44d
aoji-tjut <245611640@qq.com>
2021-04-06 17:39:28 +0800

    4.6

---
+++ C++/6.Linux系统编程/8.网络编程/udp/multicast/receiver.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+//终端运行 输入port
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        fprintf(stderr, "arg error\n");
+        exit(-1);
+    }
+
+    //获取socket
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    //加入多播组
+    struct ip_mreqn mreq;
+    inet_pton(AF_INET, "224.2.2.2", &mreq.imr_multiaddr);   //任意多播地址 224.0.0.1相当于广播
+    inet_pton(AF_INET, "0.0.0.0", &mreq.imr_address);       //本地地址
+    mreq.imr_ifindex = if_nametoindex("lo");                //网卡索引
+    setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+
+    //绑定地址
+    struct sockaddr_in local_addr;                          //本地地址结构体
+    local_addr.sin_family = AF_INET;                        //协议族
+    local_addr.sin_port = htons(atoi(argv[1]));             //主机字节序转网络字节序
+    inet_pton(AF_INET, "0.0.0.0", &local_addr.sin_addr);    //把点分十进制地址转化为网络字节序地址 0.0.0.0自动识别本地ip
+    bind(fd, (void*) &local_addr, sizeof(local_addr));
+
+    //通信
+    char buf[BUFSIZ];
+    struct sockaddr_in remote_addr;             //对方地址结构体
+    socklen_t addr_len = sizeof(remote_addr);   //对方地址结构体大小 初始化很重要
+    char remote_ip[32];                         //对方主机字节序ip
+    int remote_port;                            //对方主机字节序端口
+    fd_set set;
+    FD_ZERO(&set);
+    while(1)
+    {
+        FD_SET(fd, &set);
+        select(fd + 1, &set, NULL, NULL, NULL);
+
+        if(FD_ISSET(fd, &set))
+        {
+            recvfrom(fd, &buf, sizeof(buf), 0, (void*) &remote_addr, &addr_len);
+
+            inet_ntop(AF_INET, &remote_addr.sin_addr, remote_ip, sizeof(remote_ip));    //把网络字节序地址转化为点分十进制地址
+            remote_port = ntohs(remote_addr.sin_port);
+            printf("[%s/%d]: %s\n", remote_ip, remote_port, buf);
+
+            if(strcmp(buf, "exit") == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
---
+++ C++/6.Linux系统编程/8.网络编程/udp/multicast/sender.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+//终端运行 输入port
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        fprintf(stderr, "arg error\n");
+        exit(-1);
+    }
+
+    //获取socket
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    //打开组播属性
+    struct ip_mreqn mreq;
+    inet_pton(AF_INET, "224.2.2.2", &mreq.imr_multiaddr);   //任意多播地址 224.0.0.1相当于广播
+    inet_pton(AF_INET, "0.0.0.0", &mreq.imr_address);       //本地地址
+    mreq.imr_ifindex = if_nametoindex("lo");                //网卡索引
+    setsockopt(fd, IPPROTO_IP, IP_MULTICAST_IF, &mreq, sizeof(mreq));
+
+    //通信
+    char buf[BUFSIZ];
+    struct sockaddr_in remote_addr;                                 //对方地址结构体
+    remote_addr.sin_family = AF_INET;                               //协议族
+    remote_addr.sin_port = htons(atoi(argv[1]));                    //主机字节序转网络字节序
+    inet_pton(AF_INET, "224.2.2.2", &remote_addr.sin_addr);         //把点分十进制地址转化为网络字节序地址
+    while(1)
+    {
+        memset(buf, '\0', sizeof(buf));
+        gets(buf);
+
+        sendto(fd, buf, sizeof(buf), 0, (void*) &remote_addr, sizeof(remote_addr));
+
+        if(strcmp(buf, "exit") == 0)
+        {
+            break;
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
