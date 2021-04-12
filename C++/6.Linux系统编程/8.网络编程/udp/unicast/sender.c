3649737062984bb1ffa295e31940a9ea89243455
aoji-tjut <55875798+aoji-tjut@users.noreply.github.com>
2021-04-12 20:27:32 +0800

    Update sender.c

--- C++/6.Linux系统编程/8.网络编程/udp/unicast/sender.c
+++ C++/6.Linux系统编程/8.网络编程/udp/unicast/sender.c
@@ -5,10 +5,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-//终端运行 输入ip、port
+//终端运行 输入port
 int main(int argc, char* argv[])
 {
-    if(argc < 3)
+    if(argc < 2)
     {
         fprintf(stderr, "arg error\n");
         exit(-1);
@@ -19,10 +19,10 @@ int main(int argc, char* argv[])
 
     //通信
     char buf[BUFSIZ];
-    struct sockaddr_in remote_addr;                     //对方地址结构体
-    remote_addr.sin_family = AF_INET;                   //协议族
-    remote_addr.sin_port = htons(atoi(argv[2]));        //主机字节序转网络字节序
-    inet_pton(AF_INET, argv[1], &remote_addr.sin_addr); //把点分十进制地址转化为网络字节序地址
+    struct sockaddr_in remote_addr;                         //对方地址结构体
+    remote_addr.sin_family = AF_INET;                       //协议族
+    remote_addr.sin_port = htons(atoi(argv[2]));            //主机字节序转网络字节序
+    inet_pton(AF_INET, “0.0.0.0”, &remote_addr.sin_addr);   //把点分十进制地址转化为网络字节序地址
     while(1)
     {
         memset(buf, '\0', sizeof(buf));
