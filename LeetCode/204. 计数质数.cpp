3c45b9d96d7152fe1ab3177405172005ce1f215f
aoji-tjut <245611640@qq.com>
2021-04-21 16:16:08 +0800

    4.21

---
+++ LeetCode/204. 计数质数.cpp
@@ -0,0 +1,26 @@
+﻿class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        if(n < 2)
+        {
+            return 0;
+        }
+
+        vector<bool> v(n, true);
+
+        for(long long i = 2; i < n; i++)
+        {
+            if(v[i])
+            {
+                for(long long j = i * i; j < n; j += i)
+                {
+                    v[j] = false;
+                }
+            }
+        }
+
+        return accumulate(v.begin(), v.end(), 0) - 2;   //减去0和1
+    }
+};
---
+++ LeetCode/剑指 Offer 65. 不用加减乘除做加法.cpp
@@ -0,0 +1,18 @@
+﻿class Solution
+{
+public:
+    int add(int a, int b)
+    {
+        //不用加法的加法运算 = 不进位加法 + 只进位加法 = 异或运算 + 与运算后向左进一位。
+        //又为了消除上式中的加号，需要用一个while循环来判断，当不再进位时跳出循环。
+        while(b != 0)
+        {
+            int not_carry = a ^b;
+            int carry = ((unsigned int)(a & b) << 1);
+            a = not_carry;
+            b = carry;
+        }
+
+        return a;
+    }
+};
