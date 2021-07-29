eb028184d0bc40266a763351ca0b650e80025f06
aoji-tjut <245611640@qq.com>
2021-07-29 22:56:21 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -26,19 +26,57 @@
 #include <climits>
 using namespace std;
 
-struct Node
+struct bbox
 {
-    char c;
-    struct Node* pri;
-    struct Node* next;
+    int classes;
+    int xmin, ymin, xmax, ymax;
+    float pro;
 };
+
+bool fun(bbox i, bbox j)
+{
+    if(i.pro > j.pro)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+
+float Iou(bbox i, bbox j)
+{
+    int h, w;
+
+    return (h * w) / ((i.xmax - i.xmin) * (i.ymax - i.ymin) + (j.xmax - j.xmin) * (j.ymax - j.ymin) - h * w) * 1.0;
+}
+
+vector<bbox>& nms(vector<bbox> v, float iou)
+{
+
+    for(int i=0;i<v.size();i++)
+    {
+        sort(v.begin(), v.begin(), fun);
+        bbox max = *v.begin();
+
+        for(int j = 1; j < v.size(); j++)
+        {
+            if(Iou(v[j], max) > iou)
+            {
+                v[j].pro=0;
+            }
+        }
+    }
+
+    return v;
+}
 
 int main()
 {
-    struct Node* q;
-    q=p->next;
-
-
 
     return 0;
 }
+
+
+
+
